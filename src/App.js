dd19aa05c795b776c4d687382264ce35f0f90e4f
admond tamang <admondtamang@gmail.com>
2020-11-20 19:18:37 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.scss";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
